34185033eddb1b6d7bcc99496fbad6878d0ed188
AndreaMargutti <marguttiandrea30@gmail.com>
2024-11-14 14:55:41 +0100

    first commit

---
+++ src/components/AppMain.vue
@@ -0,0 +1,36 @@
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return {
+        apiUril : 'http://127.0.0.1:8000/api/projects',
+        postlist : [],
+    }
+  },
+
+  methods: {
+    getProjects(){
+        axios.get(this.apiUril)
+            .then(function (response) {
+                // handle success
+                console.log(response.data.results);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+    }
+  },
+
+  created(){
+    this.getProjects();
+  }
+}
+</script>
+
+<template>
+    <h1>AppMain</h1>
+</template>
+
+<style scoped>
+</style>
