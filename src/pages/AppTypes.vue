c17efeb3cbcafb391b30d9ec86c2b8a5266cb2be
AndreaMargutti <marguttiandrea30@gmail.com>
2024-11-19 22:55:21 +0100

    add types pages

--- src/pages/AppTypes.vue
+++ src/pages/AppTypes.vue
@@ -1,16 +1,38 @@
 <script>
+import axios from 'axios';
 export default {
   data() {
     return {
-      
+      apiUrl : "http://127.0.0.1:8000/api/types",
+      types : [],
     }
+  },
+  methods: {
+    getTypes(){
+      axios.get(this.apiUrl)
+        .then(response => {
+          // handle success
+          this.types = response.data.results;
+          // console.log(this.types);
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+        })
+      }
+  },
+
+  created(){
+    this.getTypes();
   }
 }
 </script>
 
 <template>
-
+  <div class="container-fluid">
+    <h1 v-for="type in types" >{{ type.name }}</h1>
+  </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 </style>
